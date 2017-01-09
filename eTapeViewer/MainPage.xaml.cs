a131cdfbe07a5d71adf6e94c090eabda0c4f22f5
Erwin Ried <erwinried@gmail.com>
2017-01-09 20:39:54 -0300

    New functionality
    -Automatically connect to tape
    -Scroll to latest added value
    -Confirmation when clearing the list
    -New functionality for Tapping, Holding and Right click
    -Receive and display device configuration

--- eTapeViewer/MainPage.xaml.cs
+++ eTapeViewer/MainPage.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel;
 using Windows.ApplicationModel.Core;
 using Windows.ApplicationModel.DataTransfer;
 using Windows.Devices.Bluetooth.GenericAttributeProfile;
@@ -13,11 +14,12 @@ using Windows.Devices.Enumeration;
 using Windows.Foundation;
 using Windows.Media.Core;
 using Windows.Storage.Streams;
+using Windows.System;
+using Windows.System.Display;
 using Windows.UI.Core;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Input;
 using eTapeViewer.Annotations;
 
@@ -25,10 +27,12 @@ namespace eTapeViewer
 {
     public sealed partial class MainPage
     {
-        private GattCharacteristic _currentDevice;
         private MediaElement _beep;
-        private ObservableCollection<MeasuredValue> _receivedValues;
         private MeasuredValue _clickedItem;
+        private GattCharacteristic _currentDevice;
+        private readonly ObservableCollection<MeasuredValue> _receivedValues;
+        private string _currentConfiguration;
+        private bool _ignoreFlyout;
 
         public MainPage()
         {
@@ -39,40 +43,48 @@ namespace eTapeViewer
 
             DataTransferManager.GetForCurrentView().DataRequested += MainPage_DataRequested;
 
-            // Do not sleep
-            new Windows.System.Display.DisplayRequest().RequestActive();
+            flyout.Closing += Flyout_Closing;
+            flyout.Opening += Flyout_Opening;
 
             if (Debugger.IsAttached)
             {
                 // Test values
                 _receivedValues.Add(new MeasuredValue(10.2));
                 _receivedValues.Add(new MeasuredValue(11.7));
-                _receivedValues.Add(new MeasuredValue(20.1) { Comments = "hello" });
+                _receivedValues.Add(new MeasuredValue(20.1) {Comments = "hello"});
             }
+
+            // Do not sleep
+            new DisplayRequest().RequestActive();
+        }
+
+        private void Flyout_Opening(object sender, object e)
+        {
+            _ignoreFlyout = true;
+            Debug.WriteLine("OPENING");
+        }
+
+        private void Flyout_Closing(Windows.UI.Xaml.Controls.Primitives.FlyoutBase sender, Windows.UI.Xaml.Controls.Primitives.FlyoutBaseClosingEventArgs args)
+        {
+            _ignoreFlyout = false;
+            Debug.WriteLine("CLOSING");
         }
 
         private void MainPage_DataRequested(DataTransferManager sender, DataRequestedEventArgs args)
         {
-            if (_receivedValues.Count>0)
+            if (_receivedValues.Count > 0)
             {
                 //var s = new StringBuilder("<style type=\"text/css\"> table.tableizer-table { font-size: 12px; border: 1px solid #CCC; font-family: Arial, Helvetica, sans-serif; } .tableizer-table td { padding: 4px; margin: 3px; border: 1px solid #CCC; } .tableizer-table th { background-color: #104E8B; color: #FFF; font-weight: bold; } </style> <table class=\"tableizer-table\"><thead><tr class=\"tableizer-firstrow\"><th>Value</th><th>Units</th><th>Comments</th></tr></thead><tbody>");
                 var s = new StringBuilder("Value,Unit,Comments" + Environment.NewLine);
 
                 foreach (var v in _receivedValues)
-                {
-                    //s.AppendLine($"<tr><td>{v.Millimeters}</td><td>mm</td><td>{v.Comments}</td></tr>");
                     s.AppendLine($"{v.Millimeters},mm,{v.Comments}");
-                }
                 //s.Append("</tbody></table>");
                 //var htmlFormat = HtmlFormatHelper.CreateHtmlFormat(s.ToString());
                 //args.Request.Data.SetHtmlFormat(htmlFormat);
                 //args.Request.Data.SetRtf(rtf)
                 args.Request.Data.SetText(s.ToString());
-                args.Request.Data.Properties.Title = "Capture - "+ Windows.ApplicationModel.Package.Current.DisplayName;
-            }
-            else
-            {
-                args.Request.FailWithDisplayText("Nothing to share");
+                args.Request.Data.Properties.Title = "Capture - " + Package.Current.DisplayName;
             }
         }
 
@@ -83,13 +95,17 @@ namespace eTapeViewer
 
         private async void ScanForDevices()
         {
-            buttonConnect.Icon = new SymbolIcon(Symbol.ZeroBars);
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                () =>
+                {
+                    buttonConnect.Icon = new SymbolIcon(Symbol.ZeroBars);
+                });
 
-            foreach(var d in await GetBluetoothDevices())
+            foreach (var d in await GetBluetoothDevices())
                 if (d.Name == "eTape")
                 {
                     ConnectToTape(d);
-                    break;
+                    //break;
                 }
         }
 
@@ -98,43 +114,87 @@ namespace eTapeViewer
             if (device == null) return; // Nothing useful here
 
             var myTape = await GattDeviceService.FromIdAsync(device.Id);
+            myTape.Device.ConnectionStatusChanged += Device_ConnectionStatusChanged;
 
-            foreach (var s in myTape.Device.GattServices)
+            if (myTape.Device.ConnectionStatus == Windows.Devices.Bluetooth.BluetoothConnectionStatus.Connected)
             {
-                // TODO: Clean the service Uuid thing
-                if (!s.Uuid.ToString().StartsWith("23455100-")) continue; // Not the right service
+                foreach (var s in myTape.Device.GattServices)
+                {
+                    // TODO: Clean the service Uuid thing
+                    if (!s.Uuid.ToString().StartsWith("23455100-")) continue; // Not the right service
 
-                foreach (var s2 in s.GetAllCharacteristics())
-                    if (s2.Uuid.ToString().StartsWith("23455102-"))
-                        if (s2.CharacteristicProperties.HasFlag(GattCharacteristicProperties.Notify))
+                    foreach (var s2 in s.GetAllCharacteristics())
+                    {
+                        var c = s2.Uuid.ToString();
+                        if (c.StartsWith("23455107-"))
                         {
-                            buttonConnect.Icon = new SymbolIcon(Symbol.FourBars);
-                            if (_currentDevice == null || _currentDevice.Uuid != s2.Uuid)
-                            {
-                                _currentDevice = s2;
-                                _currentDevice.ValueChanged += S_ValueChanged;
-                            }
-                            break;
+                            // Tape configuration
+                            var r = await s2.ReadValueAsync(Windows.Devices.Bluetooth.BluetoothCacheMode.Uncached);
+
+                            var b = DataReader.FromBuffer(r.Value);
+
+                            var bytes = new byte[b.UnconsumedBufferLength];
+                            b.ReadBytes(bytes);
+
+                            var bits = new BitArray(bytes);
+                            _currentConfiguration = "Device configuration\n" +
+                                                    $"Battery low: {bits[15]}\n" +
+                                                    $"Inside measurement: {bits[14]}\n" +
+                                                    $"Offset measurement: {bits[13]}\n" +
+                                                    $"Centerline: {bits[12]}\n" +
+                                                    $"Display units: {(bits[2] ? "Metric" : (bits[0] && bits[1] ? "Decimal feet" : (bits[1] ? "Decimal inches" : (bits[0] ? "Fractional inches" : "Feet and inches"))))}\n\n"+
+                                                    "This information will be updated when connecting to the tape. The application uses Metric and ignores all the other settings.";
                         }
+
+                        if (c.StartsWith("23455102-"))
+                            if (s2.CharacteristicProperties.HasFlag(GattCharacteristicProperties.Notify))
+                            {
+                                await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(
+                                        CoreDispatcherPriority.Normal,
+                                        () =>
+                                        {
+                                            buttonConnect.Icon = new SymbolIcon(Symbol.FourBars);
+                                        });
+
+                                if ((_currentDevice == null) || (_currentDevice.Uuid != s2.Uuid))
+                                {
+                                    _currentDevice = s2;
+                                    _currentDevice.ValueChanged += S_ValueChanged;
+                                }
+                                //break;
+                            }
+                    }
+                }
             }
+        }
+
+        private void Device_ConnectionStatusChanged(Windows.Devices.Bluetooth.BluetoothLEDevice sender, object args)
+        {
+            ScanForDevices();
         }
 
         private static async Task<DeviceInformationCollection> GetBluetoothDevices()
         {
-            return await DeviceInformation.FindAllAsync(GattDeviceService.GetDeviceSelectorFromUuid(GattServiceUuids.DeviceInformation), null);
+            return
+                await
+                    DeviceInformation.FindAllAsync(
+                        GattDeviceService.GetDeviceSelectorFromUuid(GattServiceUuids.DeviceInformation), null);
         }
 
-        private void S_ValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args)
+        private async void S_ValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args)
         {
             var b = DataReader.FromBuffer(args.CharacteristicValue);
 
             var bytes = new byte[b.UnconsumedBufferLength];
             b.ReadBytes(bytes);
 
-            CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
                 () =>
                 {
-                    _receivedValues.Add(new MeasuredValue(ConvertToMillimeters(bytes)));
+                    var m = new MeasuredValue(ConvertToMillimeters(bytes));
+                    _receivedValues.Add(m);
+                    listViewValues.ScrollIntoView(m);
+
                     sendButton.IsEnabled = true;
                     clearButton.IsEnabled = true;
 
@@ -150,12 +210,20 @@ namespace eTapeViewer
             return bytes.Length == 2 ? BitConverter.ToInt16(bytes, 0)*0.396875 : -1;
         }
 
-        private void buttonClear_Click(object sender, RoutedEventArgs e)
+        private async void buttonClear_Click(object sender, RoutedEventArgs e)
         {
-            _receivedValues.Clear();
+            var d = new MessageDialog("Delete all measurements?");
+            var yes = new UICommand("Yes");
+            d.Commands.Add(yes);
+            d.Commands.Add(new UICommand("No"));
 
-            clearButton.IsEnabled = false;
-            sendButton.IsEnabled = false;
+            if (await d.ShowAsync() == yes)
+            {
+                _receivedValues.Clear();
+
+                clearButton.IsEnabled = false;
+                sendButton.IsEnabled = false;
+            }
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
@@ -183,32 +251,34 @@ namespace eTapeViewer
                 TextWrapping = TextWrapping.Wrap,
                 PlaceholderText = "Details about this entry",
                 //Text = v.Comments ?? "",
-                SelectedText = v.Comments ?? "",
+                SelectedText = v.Comments ?? ""
             });
 
             dialog.Content = panel;
-
+            
             dialog.PrimaryButtonText = "Save";
             dialog.SecondaryButtonText = "Cancel";
 
             if (await dialog.ShowAsync() == ContentDialogResult.Primary)
-                v.Comments = ((TextBox)((StackPanel)dialog.Content).Children[0]).Text;
+                v.Comments = ((TextBox) ((StackPanel) dialog.Content).Children[0]).Text;
         }
 
         private void CopyItem_Click(object sender, RoutedEventArgs e)
         {
-            CopyToClipboard(GetMeasuredValue(e),false);
+            CopyToClipboard(GetMeasuredValue(e), false);
         }
 
         private MeasuredValue GetMeasuredValue(RoutedEventArgs e)
         {
-            return (MeasuredValue)((MenuFlyoutItem)e.OriginalSource).DataContext;
+            return (MeasuredValue) (e is ItemClickEventArgs
+                ?  ((ItemClickEventArgs) e).ClickedItem
+                : ((FrameworkElement) e.OriginalSource).DataContext);
         }
 
         private static void CopyToClipboard(MeasuredValue m, bool copyValue)
         {
             var d = new DataPackage();
-            d.SetText(copyValue?m.Millimeters+"":m.Output);
+            d.SetText(copyValue ? m.Millimeters + "" : m.Output);
             Clipboard.SetContent(d);
         }
 
@@ -234,37 +304,54 @@ namespace eTapeViewer
             DataTransferManager.ShowShareUI();
         }
 
-        private void listViewValues_ItemClick(object sender, ItemClickEventArgs e)
-        {
-            _clickedItem = (MeasuredValue) e.ClickedItem;
-        }
-
-        private void listViewValues_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-            ShowFlyout(e.OriginalSource as FrameworkElement, e.GetPosition(e.OriginalSource as UIElement));
-        }
-
         private void listViewValues_RightTapped(object sender, RightTappedRoutedEventArgs e)
         {
-            ShowFlyout(e.OriginalSource as FrameworkElement, e.GetPosition(e.OriginalSource as UIElement));
+            Debug.WriteLine("RIGHT TAP");
+            if (!_ignoreFlyout)
+            {
+                ShowFlyout(e.OriginalSource as FrameworkElement, e.GetPosition(e.OriginalSource as UIElement));
+                Debug.WriteLine("RIGHT TAP FLYOUT");
+            }
         }
 
         private void ShowFlyout(FrameworkElement f, Point p)
         {
             if (f?.DataContext != null)
-                flyout.ShowAt(f,p);
+                flyout.ShowAt(f, p);
+        }
+
+        private async void DeviceInfoButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new MessageDialog(string.IsNullOrEmpty(_currentConfiguration)?"No information available.":_currentConfiguration);
+            await dialog.ShowAsync();
+        }
+
+        private void listViewValues_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            AddCommentsItem_Click(sender, e);
+        }
+
+        private void listViewValues_Holding(object sender, HoldingRoutedEventArgs e)
+        {
+            Debug.WriteLine("HOLDING");
+            if (!_ignoreFlyout)
+            {
+                ShowFlyout(e.OriginalSource as FrameworkElement, e.GetPosition(e.OriginalSource as UIElement));
+                Debug.WriteLine("HOLDING FLYOUT");
+            }
         }
     }
 
     public class MeasuredValue : INotifyPropertyChanged
     {
         private string _comments;
-        public double Millimeters { get; set; }
 
         public MeasuredValue(double millimeters)
         {
             Millimeters = millimeters;
         }
+
+        public double Millimeters { get; set; }
 
         public string Output
         {
