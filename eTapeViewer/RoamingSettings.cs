0b4d0ab2c65ada14cac4389dd67bc147bf4f4912
Erwin Ried <erwinried@gmail.com>
2017-01-16 23:04:34 -0300

    Class to handle settings

---
+++ eTapeViewer/RoamingSettings.cs
@@ -0,0 +1,30 @@
+﻿namespace eTapeViewer
+{
+    internal static class RoamingSettings
+    {
+        public static int GetInt(Settings s)
+        {
+            var r = Windows.Storage.ApplicationData.Current.RoamingSettings.Values[s.ToString()];
+
+            if (r is int)
+                return (int)r;
+
+            return 0;
+        }
+
+        public static void IncrementInt(Settings s)
+        {
+            SetInt(s,GetInt(s) + 1);
+        }
+
+        internal static void SetInt(Settings s, int i)
+        {
+            Windows.Storage.ApplicationData.Current.RoamingSettings.Values[s.ToString()] = i;
+        }
+
+        public static void DecrementInt(Settings s)
+        {
+            SetInt(s, GetInt(s) - 1);
+        }
+    }
+}
